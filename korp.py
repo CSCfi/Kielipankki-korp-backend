135ef6235735298667141331313dda72dca2ee90
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2022-09-27 13:16:42 +0300

    korp.py: /corpus_config: One level of corpus subdirectories

--- korp.py
+++ korp.py
@@ -3444,10 +3444,21 @@ def get_mode(mode_name: str, corpora: list, cache: bool):
             file_path = Path(config.CORPUS_CONFIG_DIR) / "corpora" / path.lower() / f"{c.lower()}.yaml"
             if file_path.is_file():
                 corpus_files.append(file_path)
+            elif not path:
+                # Try corpora subdirectories (one level only)
+                file_path = glob.glob(os.path.join(
+                    config.CORPUS_CONFIG_DIR, "corpora", "*", f"{c.lower()}.yaml"))
+                if file_path:
+                    corpus_files.extend(file_path)
+                else:
+                    warnings.add(f"The corpus {c!r} does not exist, or does not have a config file.")
             else:
                 warnings.add(f"The corpus {c!r} does not exist, or does not have a config file.")
     else:
         corpus_files = glob.glob(os.path.join(config.CORPUS_CONFIG_DIR, "corpora", "*.yaml"))
+        # Add files in corpora subdirectories (one level)
+        corpus_files.extend(
+            glob.glob(os.path.join(config.CORPUS_CONFIG_DIR, "corpora", "*", "*.yaml")))
 
     # Go through all corpora to see if they are included in mode
     for corpus_file in corpus_files:
