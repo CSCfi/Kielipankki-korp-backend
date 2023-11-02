8474d2cada07ed1e5534cd7676e3f7d176074f43
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2023-11-02 12:05:37 +0200

    korpplugins.test1: Add /info1 calling korp.views.info
    korpplugins.test1:
    - Add route /info1 with a view function calling korp.views.info and
      slightly modifying the result.

--- korpplugins/test1/__init__.py
+++ korpplugins/test1/__init__.py
@@ -9,6 +9,8 @@ Korp test plugin: endpoint /test and a result wrapper in a package.
 import functools
 
 from korp import pluginlib, utils
+# Rename info to views_info to avoid a clash with korpplugins.test1.info
+from korp.views import info as views_info
 
 
 pluginconf = pluginlib.get_plugin_config(
@@ -78,6 +80,20 @@ def count2(args):
     """Yield arguments wrapped in ARGS_NAME."""
     print("test1.count2")
     yield {pluginconf["ARGS_NAME"]: args}
+
+
+@test_plugin.route("/info1")
+@utils.main_handler
+@test_decor
+def info1(args):
+    """Yield arguments wrapped in ARGS_NAME, result of /info in "result".
+
+    This is an example of calling the view function of an existing
+    endpoint from the one of a new endpoint.
+    """
+    print("test1.info1")
+    yield {pluginconf["ARGS_NAME"]: args,
+           "result": next(views_info.info(args))}
 
 
 class Test1b(pluginlib.CallbackPlugin):
