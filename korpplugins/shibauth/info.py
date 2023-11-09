9127267280dd865ee0df19128eab9806e01c1ab9
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2023-11-09 12:15:00 +0200

    korpplugins.shibauth: Implement /authenticate, ShibAuth
    korpplugins.contenthider:
    - Implement endpoing /authenticate and class ShibAuth (subclass of
      BaseAuthorizer), copying code from Språkbanken's plugins.sbauth (that
      now implements /authenticate), instead of implementing callbacks for
      filter_result and filter_auth_postdata for /authenticate.
    - Convert to use korp.pluginlib instead of korppluginlib.
    - Also otherwise adapt to the Korp backend split into modules.

--- korpplugins/shibauth/info.py
+++ korpplugins/shibauth/info.py
@@ -7,7 +7,7 @@ Information on the shibauth Korp plugin.
 
 
 NAME = "Shibboleth authorization"
-VERSION = "0.1"
-DATE = "2021-03-24"
+VERSION = "0.2"
+DATE = "2023-11-09"
 AUTHOR = "Jyrki Niemi / FIN-CLARIN"
 DESCRIPTION = "Authorization with information obtained from Shibboleth authentication."
