b75d0d7d11cf04ee55f443628da8ff04b8c9c93c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2023-11-20 14:33:03 +0200

    test_query: Add tests for /query with MAX_KWIC_ROWS

--- tests/functional/test_query.py
+++ tests/functional/test_query.py
@@ -34,6 +34,29 @@ def query_testcorpus(client):
     yield _query_testcorpus
 
 
+@pytest.fixture
+def query_testcorpus_kwic_rows(query_testcorpus):
+    """Yield a function to test the effect of MAX_KWIC_ROWS.
+
+    The returned function takes as its parameters the value for
+    MAX_KWIC_ROWS and the number of rows to request. It returns the
+    JSON response for /query to corpus "testcorpus" with CQP query
+    "[]" (any word) from the beginning of the corpus (start=0).
+    """
+
+    def _query_testcorpus_kwic_rows(max_rows, request_rows):
+        return query_testcorpus(
+            "[]",
+            {
+                "start": "0",
+                "end": str(request_rows - 1),
+            },
+            {"MAX_KWIC_ROWS": max_rows}
+        )
+
+    yield _query_testcorpus_kwic_rows
+
+
 class TestQuery:
 
     """Tests for /query"""
@@ -45,3 +68,24 @@ class TestQuery:
         assert len(kwic) == data["hits"]
         # print(data)
         # assert 0
+
+    def test_query_max_kwic_rows(self, query_testcorpus_kwic_rows):
+        """Test a query requesting MAX_KWIC_ROWS results."""
+        num = 1
+        data = query_testcorpus_kwic_rows(num, num)
+        assert len(data["kwic"]) == num
+
+    def test_query_max_kwic_exceeded(self, query_testcorpus_kwic_rows):
+        """Test a query requesting MAX_KWIC_ROWS + 1 results."""
+        num = 1
+        data = query_testcorpus_kwic_rows(num, num + 1)
+        errmsg = f"At most {num} KWIC rows can be returned per call."
+        assert "ERROR" in data and errmsg in data["ERROR"]["value"]
+
+    def test_query_max_kwic_unlimited(self, query_testcorpus_kwic_rows):
+        """Test a query with MAX_KWIC_ROWS = 0."""
+        # testcorpus does not contain 1,000,000 tokens, so the
+        # following should return all hits. MAX_KWIC_ROWS is tested
+        # before returning the data.
+        data = query_testcorpus_kwic_rows(0, 1000000)
+        assert len(data["kwic"]) == data["hits"]
