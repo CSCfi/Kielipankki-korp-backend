ec250614cf5fa1c83cb73596e9438e0954693cf7
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2023-12-08 13:12:28 +0200

    tests/functional: Db tests: Use fixture database_tables
    tests/functional/test_lemgram_count.py,
    tests/functional/test_timespan.py:
    - Add argument "corpus" to the function returned by a local fixture.
    - Use fixture database_tables and load database data only for the
      corpora actually used.
    - Use tests.testutils.make_liststr to convert a list of corpora to a
      string.

--- tests/functional/test_lemgram_count.py
+++ tests/functional/test_lemgram_count.py
@@ -8,40 +8,42 @@ Pytest tests for the Korp /lemgram_count endpoint
 
 import pytest
 
-from tests.testutils import get_response_json
+from tests.testutils import get_response_json, make_liststr
 
 
 @pytest.fixture
-def lemgram_count_testcorpus(client, database):
-    """Yield function returning JSON response for /lemgram_count to testcorpus.
+def lemgram_count(client, database_tables):
+    """Yield function returning JSON response for /lemgram_count to corpora.
 
-    The returned function takes as its parameters a lemgram, possible
-    additional request parameters and Korp configuration parameters.
-    It returns the JSON response for /lemgram_count to corpus
-    "testcorpus1" with the given parameters (and cache=false). It imports
+    The returned function takes as its parameters a lemgram, a corpus
+    or a list of corpora, possible additional request parameters and
+    Korp configuration parameters.
+    It returns the JSON response for /lemgram_count to the specified
+    corpora with the given parameters (and cache=false). It imports
+    the lemgram_index database data for the given corpora.
     """
 
-    def _lemgram_count_testcorpus(lemgram, params=None, config=None):
+    def _lemgram_count(lemgram, corpora, params=None, config=None):
         query_params = {
-            "corpus": "testcorpus1",
+            "corpus": make_liststr(corpora),
             "lemgram": lemgram,
             "cache": "false",
         }
-        # Import all lemgram_index table data
-        database.import_table_files(["lemgram_index/*.tsv"])
+        database_tables(corpora, "lemgram_index")
         query_params.update(params or {})
         return get_response_json(
             client(config or {}), "/lemgram_count", query_string=query_params)
 
-    yield _lemgram_count_testcorpus
+    yield _lemgram_count
 
 
 class TestLemgramCount:
 
     """Tests for /lemgram_count"""
 
-    def test_lemgram_count_single_corpus(self, lemgram_count_testcorpus):
+    def test_lemgram_count_single_corpus(self, lemgram_count):
         """Test /lemgram_count on a single corpus and single lemgram."""
         lemgram = "test..nn.1"
-        data = lemgram_count_testcorpus(lemgram)
+        corpus = "testcorpus1"
+        data = lemgram_count(lemgram, corpus)
         assert lemgram in data
